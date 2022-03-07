486035848577682c36d56c831949553f1cc39dc9
Syed Rafey Husain <rafey.husain@gmail.com>
2022-03-07 16:36:00 +0500

    Basic frontend with CORS working

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,21 @@ function App() {
   const [ count, setCount ] = useState(0)
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(response => response.json())
-      .then(response => console.log(response))
+    get_counter()
   }, [])
+
+  const get_counter = () => {
+    fetch('http://127.0.0.1:8000/api/counter')
+    .then(response => response.json())
+    .then(json => {
+      setCount(+json.value)
+      console.log(json)
+    })
+  }
 
   return (
     <>
-      <button onClick={() => setCount(count + 1)}>{count}</button>
+      <button onClick={() => get_counter()}>{count}</button>
     </>
   );
 }
--- server/routes/api.php
+++ server/routes/api.php
@@ -19,6 +19,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group([], function () {
+Route::group([ 'middleware' => ['cors']], function () {
     Route::apiResource('counter', CounterController::class);
 });
