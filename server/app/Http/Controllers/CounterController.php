b1e5c85fa5859ecbedfb756cd33c7dccf3fbc3e1
Syed Rafey Husain <rafey.husain@gmail.com>
2022-03-07 16:18:42 +0500

    Backend api logic completed

--- server/app/Http/Controllers/CounterController.php
+++ server/app/Http/Controllers/CounterController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Carbon\Carbon;
+use DateTime;
+use App\Counter;
 
 class CounterController extends Controller
 {
@@ -13,5 +17,40 @@ class CounterController extends Controller
      */
     public function index()
     {
+        try {
+            $rows = Counter::orderBy('created_at', 'DESC')->take(1)->get();
+            $value = 1;
+
+            if (count($rows) == 0) {
+                $this->new();
+            } else {
+                $row = $rows[0];
+                $now = new DateTime(Carbon::now());
+                $latest = new DateTime($row["created_at"]);
+                $interval = $now->diff($latest);
+
+                if($interval->days > 0) {
+                    $this->new();
+                } else {
+                    $value = $row["value"] + 1;
+                
+                    $this->update($row, $value);
+                }
+            }
+
+            return ['value' => $value];
+
+        } catch (\Exception $exception) {
+            throw new HttpException(400, "Invalid data - {$exception->getMessage()}");
+        }
+    }
+
+    private function new() {
+        $counter = new Counter;
+        $counter->fill(['value' => 1, 'timestamp' => Carbon::now()->timestamp])->save();
+    }
+
+    private function update($counter, $value) {
+        $counter->fill(['value' => $value])->save();
     }
 }
--- server/database/migrations/2022_03_07_091001_create_counters_table.php
+++ server/database/migrations/2022_03_07_091001_create_counters_table.php
@@ -14,6 +14,7 @@ class CreateCountersTable extends Migration
     public function up()
     {
         Schema::create('counters', function (Blueprint $table) {
+            $table->bigIncrements('id');
             $table->bigInteger('value');
             $table->timestamps();
         });
