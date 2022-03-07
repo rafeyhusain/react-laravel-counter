cac275f284b1e3e576ab1c008c0b2f7762193bf2
Syed Rafey Husain <rafey.husain@gmail.com>
2022-03-07 16:51:40 +0500

    CSS Styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
-  const [ count, setCount ] = useState(0)
+  const [count, setCount] = useState(0)
 
   useEffect(() => {
     get_counter()
@@ -9,16 +10,16 @@ function App() {
 
   const get_counter = () => {
     fetch('http://127.0.0.1:8000/api/counter')
-    .then(response => response.json())
-    .then(json => {
-      setCount(+json.value)
-      console.log(json)
-    })
+      .then(response => response.json())
+      .then(json => {
+        setCount(+json.value)
+      })
   }
 
   return (
     <>
-      <button onClick={() => get_counter()}>{count}</button>
+      <h1>{count}</h1>
+      <button className='neutral' onClick={() => get_counter()}>Click Me!</button>
     </>
   );
 }
